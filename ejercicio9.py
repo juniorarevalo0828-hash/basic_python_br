ad954406ce822428cfe702cc68ea1ac03d5ebc27
juniorarevalo0828-hash <juniorarevalo0828@gmail.com>
2026-09-01 18:03:28 -0500

    Update ejercicio9.py

--- ejercicio9.py
+++ ejercicio9.py
@@ -1,1 +1,13 @@
+try:
+    numero1 = float(input("Ingrese el primer número: "))
+    numero2 = float(input("Ingrese el segundo número: "))
 
+    resultado = numero1 / numero2
+
+    print("El resultado de la división es:", resultado)
+
+except ZeroDivisionError:
+    print("Error: No se puede dividir entre cero.")
+
+except ValueError:
+    print("Error: Debe ingresar números válidos.")
