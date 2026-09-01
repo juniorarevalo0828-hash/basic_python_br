20ff95e972d1095f23dca848b9bd3521ea337fdf
juniorarevalo0828-hash <juniorarevalo0828@gmail.com>
2026-09-01 17:59:44 -0500

    Update ejercicio3.py

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,1 +1,8 @@
+numero = int(input("Ingrese un número entero: "))
 
+if numero % 2 == 0:
+    print("El número es par")
+else:
+    siguiente_par = numero + 1
+    print("El número es impar")
+    print("El siguiente número par es:", siguiente_par)
