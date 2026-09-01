8f9aa2a3a8cc20fabd1402807a127937c1df5c4f
juniorarevalo0828-hash <juniorarevalo0828@gmail.com>
2026-09-01 18:07:50 -0500

    Update ejercicio13.py

--- ejercicio13.py
+++ ejercicio13.py
@@ -1,1 +1,12 @@
+try:
+    with open("datos.txt", "r", encoding="utf-8") as archivo:
+        contenido = archivo.read()
 
+        print("Contenido del archivo:")
+        print(contenido)
+
+except FileNotFoundError:
+    print("Error: El archivo datos.txt no fue encontrado.")
+
+except Exception as error:
+    print("Ocurrió un error:", error)
