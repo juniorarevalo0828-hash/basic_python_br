453c06dd1e8ab33c4c5c38c122a52831478066e3
juniorarevalo0828-hash <juniorarevalo0828@gmail.com>
2026-09-01 18:04:01 -0500

    Update ejercicio10.py

--- ejercicio10.py
+++ ejercicio10.py
@@ -1,1 +1,6 @@
+numeros = list(range(1, 11))
 
+print("Lista completa:", numeros)
+print("Primer elemento:", numeros[0])
+print("Último elemento:", numeros[-1])
+print("Cantidad de elementos:", len(numeros))
