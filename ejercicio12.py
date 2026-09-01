1c8ddbd6234df796eb1b48b8317605a24e6e2f72
juniorarevalo0828-hash <juniorarevalo0828@gmail.com>
2026-09-01 18:07:27 -0500

    Update ejercicio12.py

--- ejercicio12.py
+++ ejercicio12.py
@@ -1,1 +1,16 @@
+def comparar_numeros(a, b):
 
+    if a > b:
+        print(a, "es mayor que", b)
+
+    elif b > a:
+        print(b, "es mayor que", a)
+
+    else:
+        print("Ambos números son iguales")
+
+
+numero1 = float(input("Ingrese el primer número: "))
+numero2 = float(input("Ingrese el segundo número: "))
+
+comparar_numeros(numero1, numero2)
