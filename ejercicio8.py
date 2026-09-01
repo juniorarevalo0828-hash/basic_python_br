b67580c1280df0b9f6a69963bd07d2be4351b2c3
juniorarevalo0828-hash <juniorarevalo0828@gmail.com>
2026-09-01 18:03:04 -0500

    Update ejercicio8.py

--- ejercicio8.py
+++ ejercicio8.py
@@ -1,1 +1,17 @@
+class Producto:
 
+    def __init__(self, nombre, precio, cantidad):
+        self.nombre = nombre
+        self.precio = precio
+        self.cantidad = cantidad
+
+    def calcular_total(self):
+        return self.precio * self.cantidad
+
+
+producto = Producto("Computador", 2500000, 2)
+
+total = producto.calcular_total()
+
+print("Producto:", producto.nombre)
+print("Valor total del inventario:", total)
