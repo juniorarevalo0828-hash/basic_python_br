82430e8614b17f282a7d9eee9c66681e909535c6
juniorarevalo0828-hash <juniorarevalo0828@gmail.com>
2026-09-01 18:08:26 -0500

    Update ejercicio14.py

--- ejercicio14.py
+++ ejercicio14.py
@@ -1,1 +1,18 @@
+import json
 
+
+persona = {
+    "nombre": "Amado",
+    "edad": 20,
+    "carrera": "Ingeniería de Sistemas y Computación",
+    "ciudad": "Barranquilla"
+}
+
+
+persona_json = json.dumps(
+    persona,
+    indent=4,
+    ensure_ascii=False
+)
+
+print(persona_json)
