45875dededb06782d356defbc348c1b2f85d87c1
juniorarevalo0828-hash <juniorarevalo0828@gmail.com>
2026-09-01 18:01:42 -0500

    Update ejercicio6.py

--- ejercicio6.py
+++ ejercicio6.py
@@ -1,1 +1,9 @@
+persona = {
+    "nombre": "Amado",
+    "edad": 20,
+    "carrera": "Ingeniería de Sistemas y Computación",
+    "ciudad": "Barranquilla"
+}
 
+print("Información de la persona:")
+print(persona)
