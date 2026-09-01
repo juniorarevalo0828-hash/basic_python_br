9d6540cd5ca64c86c89471b9ee6ae98c509cce11
juniorarevalo0828-hash <juniorarevalo0828@gmail.com>
2026-09-01 18:02:37 -0500

    Update ejercicio7.py

--- ejercicio7.py
+++ ejercicio7.py
@@ -1,1 +1,18 @@
+persona = {
+    "nombre": "Amado",
+    "edad": 20,
+    "carrera": "Ingeniería de Sistemas y Computación",
+    "ciudad": "Barranquilla"
+}
 
+print("CLAVES:")
+for clave in persona.keys():
+    print(clave)
+
+print("\nVALORES:")
+for valor in persona.values():
+    print(valor)
+
+print("\nCLAVE - VALOR:")
+for clave, valor in persona.items():
+    print(clave, ":", valor)
